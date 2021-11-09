a729044e9007249dbf4b42450384620e89839906
rest <shkerdin.v@yandex.ru>
2021-11-10 01:53:51 +0300

    some addition

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import moment from 'moment';
 import fp from 'lodash/fp';
 
+import { someFunc } from 'Lib/index';
 import { ExampleComponent } from 'Components/ExampleComponent';
-
-import styles from '../styles/Home.module.scss';
+import styles from 'styles/Home.module.scss';
 
 
 const Home: NextPage = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data: any) => console.log(fp.get(data));
+    const {
+        register,
+        handleSubmit,
+    } = useForm();
+    const onSubmit = (data: any) => console.log(fp.get('firstName', data), someFunc());
+
+    const [time] = useState(moment().minute(0).second(0).format('dddd, MMMM Do YYYY, hh:mm:ss'));
 
     return (
         <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
@@ -22,7 +27,7 @@ const Home: NextPage = () => {
             <br />
             <ExampleComponent />
             <br />
-            {moment().format('dddd, MMMM Do YYYY, hh:mm:ss')}
+            {time}
         </form>
     );
 };
